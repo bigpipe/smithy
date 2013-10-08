4edb97f92d8920312655b351e56a97b628953ac6
Swaagie <info@martijnswaagman.nl>
2013-10-08 12:50:20 +0200

    [test] importer function

--- test/common.js
+++ test/common.js
@@ -7,6 +7,7 @@ chai.Assertion.includeStack = true;
 // Expose smithy
 //
 exports.smithy = require('../');
+exports.import = require('../import');
 exports.fs = require('fs');
 exports.canihaz = require('canihaz')('smithy');
 
---
+++ test/import.test.js
@@ -0,0 +1,28 @@
+describe('Importer', function () {
+  'use strict';
+
+  var common = require('./common')
+    , smithy = common.smithy
+    , fs = common.fs
+    , expect = common.expect
+    , importer = common.import
+    , standard = importer();
+
+  it('returns proper import function', function () {
+    expect(importer).to.be.a('function');
+  });
+
+  it('returns the paths of imported files', function () {
+    var result = standard(__dirname + '/fixtures/less.less');
+
+    expect(result).to.be.an('array');
+    expect(result).to.include('/home/swaagie/projects/smithy/test/fixtures/imports/less.less');
+    expect(result).to.have.length(1);
+  });
+
+  it('returns empty array if there are no files to import', function () {
+    var result = standard(__dirname + '/fixtures/stylus.stylus');
+    expect(result).to.be.an('array');
+    expect(result).to.have.length(0);
+  });
+});
